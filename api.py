6fcad7ba4c5b040bbc25e39a5aab8640db1f5aa6
Samuel Machado <samuel_emerenciano@estudante.sesisenai.org.br>
2025-12-02 11:48:09 -0300

    q

--- api.py
+++ api.py
@@ -1,12 +1,21 @@
 from fastapi import FastAPI
 import main
+import banco as b
 
+colab = [
+    {
+        'nome': 'Açucena Pereira',
+        'cpf': '123.456.789-09',
+        'endereço': 'Rua da Alegria',
+        'telefone': '(47) 99505-0565'
+    }
+]
 
 
 app = FastAPI()
 
 
-dados = main.us()
+# dados = main.us()
 
 users = []
 
@@ -14,11 +23,25 @@ users = []
 
 @app.get("/")
 def home():
-    return "Olá mundo"
+    return colab
 
-@app.post("/")
-def add_user(dados):
-    users.append(dados)
-    return users
+@app.post("/funcionarios")
+def ver_funcionario():
+    nome = colab['nome']
+    cpf = colab['cpf']
+    end = colab['endereço']
+    tel = colab['telefone']
+
+    b.func(nome, cpf, end, tel)
+
+
+
+
+    colab
+
+# @app.post("/")
+# def add_user(dados):
+#     users.append(dados)
+#     return users
 
 
---
+++ banco.py
@@ -0,0 +1,28 @@
+import mysql.connector
+
+con = mysql.connector.connect(
+    host="localhost",
+    user="root",        # ou o usuário que você usa
+    password="geleiadoce",
+    database="mercadinho"
+)
+
+texto = "Salvo com sucesso!"
+
+if con.is_connected():
+    print("Conectado!")
+
+cursor = con.cursor()
+
+
+
+
+def func(n, c, e, t):
+    comando = """INSERT INTO funcionario (Nome, CPF, Endereco, Telefone) VALUES (%s, %s, %s, %s)"""
+
+    cursor.execute(comando, (n, c, e, t))
+    con.commit()
+    return texto
+
+
+con.close()
